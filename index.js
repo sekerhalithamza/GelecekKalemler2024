95f032fa5e9ce96eb5f6bb005ddad7215b23eddf
Halit Hamza Şeker <shalithamza@gmail.com>
2023-09-21 19:18:50 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// MODULES
+const express = require("express");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+// LOCAL VARIABLES
+const app = express();
