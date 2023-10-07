95b737ea0d72216e803c025df8baa35f78b3e183
Halit Hamza Şeker <shalithamza@gmail.com>
2023-10-07 21:57:38 +0300

    Add: contact

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,12 @@
 "use strict";
 
-const containers = document.getElementsByClassName("faq-container");
+const faqContainers = document.getElementsByClassName("faq-container");
 
-for (const container of containers) {
+for (const container of faqContainers) {
   container.addEventListener("click", () => {
     container.classList.toggle("open");
   });
 }
+
+const contactInput = document.getElementById("contact-input");
+const contactButton = document.getElementById("contact-btn");
