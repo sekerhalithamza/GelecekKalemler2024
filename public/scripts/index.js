d58719dec010708a703577cb8ee135ad3c4473d1
Halit Hamza Şeker <shalithamza@gmail.com>
2024-01-10 13:06:46 +0300

    Fixed navbtn in application page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -10,3 +10,20 @@ for (const container of faqContainers) {
 
 const contactInput = document.getElementById("contact-input");
 const contactButton = document.getElementById("contact-btn");
+
+const upBtn = document.getElementById("upBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > window.innerHeight / 2) {
+    upBtn.style.display = "grid";
+  } else {
+    upBtn.style.display = "none";
+  }
+});
+
+upBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,20 +1,3 @@
-const upBtn = document.getElementById("upBtn");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > window.innerHeight / 2) {
-    upBtn.style.display = "grid";
-  } else {
-    upBtn.style.display = "none";
-  }
-});
-
-upBtn.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
-
 const btnNav = document.getElementById("btnNav");
 const nav = document.getElementById("nav");
 
